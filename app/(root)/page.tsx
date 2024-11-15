9e93fc72239931d166bd73f14e0f26eb7b80f0e4
Ilya <ilya671qw@gmail.com>
2024-11-15 18:20:02 +0300

    installed sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query;
+  
+  const posts =[{
+    _createdAt: new Date(),
+    views: 55,
+    author: { _id: 1, name: 'John' },
+    _id: 1,
+    description: 'This is a description',
+    image: "https://images.pexels.com/photos/9589211/pexels-photo-9589211.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    category: "Random",
+    title: "Hungarian Parliament Building in Budapest"
+  }]
 
   return (
     <>
@@ -16,7 +28,22 @@ export default async function Home({ searchParams }: {
         </p>
 
         <SearchForm query={query}/>
+      </section>
 
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            )) 
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
