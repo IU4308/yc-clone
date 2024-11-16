715b588e91175e89a94ea55701dad04addedaba9
Ilya <ilya671qw@gmail.com>
2024-11-16 13:36:46 +0300

    finished ppr handling

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -8,6 +8,8 @@ export const experimental_ppr = true;
 const page = async ({ params }: { params: Promise<{ id: string }>}) => {
   const id = (await params).id
 
+  console.log({id})
+
   const post = await client.fetch(STARTUP_BY_ID_QUERY, { id })
 
   if (!post) return notFound()
