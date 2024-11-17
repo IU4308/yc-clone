1e6aa196036e89c66692bac1a2097e3ee3f90a74
Ilya <ilya671qw@gmail.com>
2024-11-17 18:10:05 +0300

    finished profile route

--- auth.ts
+++ auth.ts
@@ -3,6 +3,19 @@ import GitHub from "next-auth/providers/github";
 import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
 import { client } from "@/sanity/lib/client";
 import { writeClient } from "@/sanity/lib/write-client";
+/* 
+type SignInProps = {
+  user: {
+    name: string,
+    email: string,
+    image: URL,
+  },
+  profile: {
+    id: number,
+    login: string,
+    bio: string,
+  }
+} */
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://cf68b7565fb55529175b998d21f6e644@o4508313779765248.ingest.de.sentry.io/4508313784483920",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
